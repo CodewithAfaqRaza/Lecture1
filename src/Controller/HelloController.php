697e049d254e5ad43a5b3ae3f037c6d6a8e2650d
afaqraza16 <afaqraza0220@gmail.com>
2025-01-22 12:39:57 +0500

    i have addeed the welcome controller and some muiltuiple functions or methods

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -2,12 +2,24 @@
 
 namespace App\Controller;
 
+use LDAP\Result;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class HelloController{
-    #[Route('/welcome')]
-    public function welcome(){
-        return new Response('Welcome to Symfony!');
+    #[Route('/welcome/{name}',name:'welcome')]
+
+    public function welcome(string $name){
+        return new Response('Welcome to Symfony!'. $name);
+    }
+    #[Route('/about')]
+
+    public function about(){
+        return new Response('About to Symfony!');
+    }
+    #[Route('myltipy/{a<\d+>}/{b<\d+>}')]
+    public function multipy( $a, $b){
+        $result = $a * $b;
+        return new Response("The result is ".$result);
     }
 }
