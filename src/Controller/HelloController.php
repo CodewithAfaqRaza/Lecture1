fb1a4549776ab2a9cab7d18737a78293c4984d93
afaqraza16 <afaqraza0220@gmail.com>
2025-01-22 16:11:21 +0500

    added examples of autowiring .

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -2,25 +2,35 @@
 
 namespace App\Controller;
 
+use App\Respository\StudentRespository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Psr\Log\LoggerInterface; 
 
 class HelloController extends AbstractController{
     #[Route('/welcome/{name}',name:'welcome',methods:'get')]
 
     public function welcome(string $name){
+
         return $this->render("hello/hello.html.twig",['name'=>$name]);
         // return new Response('Welcome to Symfony!'. $name);
     }
     #[Route('/about')]
-
-    public function about(){
+// 
+    public function about(LoggerInterface $logger){
+        $logger->info("We have just access the About Us page");
         return $this->render("hello/about.html.twig");
     }
-    #[Route('multiply/{a<\d+>}/{b<\d+>}',name:"multiply")]
+    #[Route('multiplyNumbers/{a<\d+>}/{b<\d+>}',name:"multiply")]
     public function multiply( $a, $b){
         $result = $a * $b;
         return $this->render("hello/multiply.html.twig",['result'=>$result]);
+    }
+    #[Route('getStudents')]
+    public function getStudent(StudentRespository $repo){
+        $students = $repo->StudentsData();
+
+        return $this->render('student/student.html.twig',['students'=>$students]);
     }
 }
