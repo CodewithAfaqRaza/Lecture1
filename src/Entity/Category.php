6cd6719d89c19a4b6170ff060f283022959cbc0d
afaqraza16 <afaqraza0220@gmail.com>
2025-01-30 13:51:40 +0500

    i have added a configuration for the database to connet to the mysql database via doctrine and i have created a category table along with its id and name columns through doctrine migraions commands ...

--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -2,10 +2,18 @@
 
 namespace App\Entity;
 
+use App\Repository\CategoryRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CategoryRepository::class)]
+
 
 class Category{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
   private ?int $id = null;
-
+    #[ORM\Column]
   private ?string $name = null;
 
   
